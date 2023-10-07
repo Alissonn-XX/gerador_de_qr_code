9a4240971aac5d15235b419a448f303a1c694cdb
Alissonn-XX <costaalisson777@gmial.com>
2023-10-07 14:05:22 -0300

    alteração

--- js/index.js
+++ js/index.js
@@ -1,68 +1,51 @@
-const form = document.querySelector('form')
-const imageQrcode = document.querySelector('[data-qrcode="qrcode-image"]')
-const conteinerBtnAcao = document.querySelector('[data-qrcode="acao-qrcode"]')
-const donwloadQrcodeBtn = document.querySelector('[data-qrcode="download-qrcode"]')
+const form = document.querySelector("form");
+const imageQrcode = document.querySelector('[data-qrcode="qrcode-image"]');
+const conteinerBtnAcao = document.querySelector('[data-qrcode="acao-qrcode"]');
+const donwloadQrcodeBtn = document.querySelector(
+  '[data-qrcode="download-qrcode"]'
+);
 
-const localHost = 'http://127.0.0.1:5500/#'
-const urlAPI = valueQR => 
-   `https://chart.apis.google.com/chart?cht=qr&chl=${valueQR}&chs=300`
+const localHost = "http://127.0.0.1:5500/#";
+const urlAPI = (valueQR) =>
+  `https://chart.apis.google.com/chart?cht=qr&chl=${valueQR}&chs=300`;
 
-
-const geradoQrCode = async valueQr =>{
-    try{
-        const entradaQR = urlAPI(valueQr)
-        const response = await fetch(entradaQR)
-        if(!response.ok){
-            throw new Error('Não foi possivel gerar o QrCode')
-        }
-        return response.url
-    }catch({error,mensage}){
-      console.log(`Erro: ${error} ${mensage}`);
+const geradoQrCode = async (valueQr) => {
+  try {
+    const entradaQR = urlAPI(valueQr);
+    const response = await fetch(entradaQR);
+    if (!response.ok) {
+      throw new Error("Não foi possivel gerar o QrCode");
     }
-}
+    return response.url;
+  } catch ({ error, mensage }) {
+    console.log(`Erro: ${error} ${mensage}`);
+  }
+};
 
-const aplicandoQrcode = async qrTexto =>{
-    if(qrTexto){
-       let QRCODE = await geradoQrCode(qrTexto)
-       imageQrcode.src = QRCODE;
-    }
-    return imageQrcode
-}
-
+const aplicandoQrcode = async (qrTexto) => {
+  if (qrTexto) {
+    let QRCODE = await geradoQrCode(qrTexto);
+    imageQrcode.src = QRCODE;
+  }
+  return imageQrcode;
+};
 
 const downloadQr = async (qrTexto) => {
-  let qrcode = donwloadQrcodeBtn.children[0];
-
-  let imageQrcode = await aplicandoQrcode(qrTexto)
-    if(imageQrcode.src.includes('https')){
-        qrcode.href = imageQrcode.src
-        qrcode.download="seu-qrcode"
+  let imageQrcode = await aplicandoQrcode(qrTexto);
+  if(imageQrcode.src.includes("https")) {
+       
     }
-  return qrcode
-}
+};
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let entradaQR = event.target.entradaQr.value;
+  downloadQr(entradaQR);
+  event.target.reset();
+});
 
-
-form.addEventListener('submit', event=>{
-  event.preventDefault()
-  let entradaQR = event.target.entradaQr.value
-  downloadQr(entradaQR)
-  event.target.reset()
-})
-
-conteinerBtnAcao.children[1].onclick = ()=>{
-   location.reload()
-}
-  
-donwloadQrcodeBtn.addEventListener('click',()=>{
-  let downloadImagenQr = donwloadQrcodeBtn.children[0];
-       if(downloadImagenQr.href === localHost){
-         alert('Não existe nenhum QrCode gerado!')
-         return
-        }
-})
-
-
-
+conteinerBtnAcao.children[1].onclick = () => {
+  location.reload();
+};
 
 
