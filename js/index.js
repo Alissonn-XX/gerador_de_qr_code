14cebe40ae191242445d255a9807d4f5949bf435
Alissonn-XX <costaalisson777@gmial.com>
2023-10-09 00:43:22 -0300

    ajustes

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const conteinerBtnAcao = document.querySelector('[data-qrcode="acao-qrcode"]');
 const donwloadQrcodeBtn = document.querySelector(
   '[data-qrcode="download-qrcode"]'
 );
+const conteinerAjuda = document.querySelector(".ajuda-qrcode");
 
 const urlAPI = (valueQr) =>
   `https://chart.apis.google.com/chart?cht=qr&chl=${valueQr}&chs=300`;
@@ -12,7 +13,7 @@ const geradoQrCode = async (valueQr) => {
   try {
     const entradaQR = urlAPI(valueQr);
     const response = await fetch(entradaQR);
-    if (!response.ok){
+    if (!response.ok) {
       throw new Error("Não foi possivel gerar o QrCode");
     }
     return response.url;
@@ -25,24 +26,23 @@ const aplicandoQrcode = async (qrTexto) => {
   if (qrTexto) {
     let QRCODE = await geradoQrCode(qrTexto);
     imagenQrcode.src = QRCODE;
-  }else{
-    alert('Para gerar um QrCode digite algo. Seja criativo ;-)');
+  } else {
+    alert("Para gerar um QrCode digite algo. Seja criativo ;-)");
   }
-  return imagenQrcode
+  return imagenQrcode;
 };
+
 let controle;
 const ativandoDownload = async (qrTexto) => {
   let imageQrcode = await aplicandoQrcode(qrTexto);
-    if(imageQrcode.src.includes("https")){
-       controle = true
-    }
+  if (imageQrcode.src.includes("https")) {
+    controle = true;
+  }
 };
-
 
 conteinerBtnAcao.children[1].onclick = () => {
   location.reload();
 };
-
 
 form.addEventListener("submit", (event) => {
   let entradaQR = event.target.entradaQr.value;
@@ -51,20 +51,26 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 });
 
-donwloadQrcodeBtn.addEventListener('click', ()=>{
-  if(!controle){
-    controle = confirm('Nenhum Qrcode foi gerado. Deseja baixar esse exmplo')
+donwloadQrcodeBtn.addEventListener("click", () => {
+  if (!controle) {
+    controle = confirm("Nenhum Qrcode foi gerado. Deseja baixar esse exmplo");
   }
-  const dowload = imagenUrl => {
-    const elementDowload = document.createElement('a')
-    fetch(imagenUrl).then( async retornoURL =>{
-      const urlBlob = await retornoURL.blob()
-      elementDowload.href = URL.createObjectURL(urlBlob)
-      elementDowload.download = 'seu-qrcode'
-      elementDowload.click()
-      URL.revokeObjectURL(elementDowload.href)
-    })
-  } 
-  return controle ? dowload(imagenQrcode.src) : false
-})
+  const dowload = (imagenUrl) => {
+    const elementDowload = document.createElement("a");
+    fetch(imagenUrl).then(async (retornoURL) => {
+      const urlBlob = await retornoURL.blob();
+      elementDowload.href = URL.createObjectURL(urlBlob);
+      elementDowload.download = "seu-qrcode";
+      elementDowload.click();
+      URL.revokeObjectURL(elementDowload.href);
+    });
+  };
+  return controle ? dowload(imagenQrcode.src) : false;
+});
 
+conteinerAjuda.children[0].addEventListener("click", () => {
+  const videoAjudaElement = document.querySelector('[data-qrcode="video-qrcode"]')
+  videoAjudaElement.classList.toggle('video-ajuda-on')
+});
+
+ 
