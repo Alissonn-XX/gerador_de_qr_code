f5501f418ae1f4133c6f3264f308bca64d6c2451
Alissonn-XX <costaalisson777@gmial.com>
2023-10-08 21:39:50 -0300

    ajustes

--- js/index.js
+++ js/index.js
@@ -1,19 +1,18 @@
 const form = document.querySelector("form");
-const imageQrcode = document.querySelector('[data-qrcode="qrcode-image"]');
+const imagenQrcode = document.querySelector('[data-qrcode="qrcode-image"]');
 const conteinerBtnAcao = document.querySelector('[data-qrcode="acao-qrcode"]');
 const donwloadQrcodeBtn = document.querySelector(
   '[data-qrcode="download-qrcode"]'
 );
 
-const localHost = "http://127.0.0.1:5500/#";
-const urlAPI = (valueQR) =>
-  `https://chart.apis.google.com/chart?cht=qr&chl=${valueQR}&chs=300`;
+const urlAPI = (valueQr) =>
+  `https://chart.apis.google.com/chart?cht=qr&chl=${valueQr}&chs=300`;
 
 const geradoQrCode = async (valueQr) => {
   try {
     const entradaQR = urlAPI(valueQr);
     const response = await fetch(entradaQR);
-    if (!response.ok) {
+    if (!response.ok){
       throw new Error("Não foi possivel gerar o QrCode");
     }
     return response.url;
@@ -25,27 +24,47 @@ const geradoQrCode = async (valueQr) => {
 const aplicandoQrcode = async (qrTexto) => {
   if (qrTexto) {
     let QRCODE = await geradoQrCode(qrTexto);
-    imageQrcode.src = QRCODE;
+    imagenQrcode.src = QRCODE;
+  }else{
+    alert('Para gerar um QrCode digite algo. Seja criativo ;-)');
   }
-  return imageQrcode;
+  return imagenQrcode
 };
-
-const downloadQr = async (qrTexto) => {
+let controle;
+const ativandoDownload = async (qrTexto) => {
   let imageQrcode = await aplicandoQrcode(qrTexto);
-  if(imageQrcode.src.includes("https")) {
-       
+    if(imageQrcode.src.includes("https")){
+       controle = true
     }
 };
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let entradaQR = event.target.entradaQr.value;
-  downloadQr(entradaQR);
-  event.target.reset();
-});
 
 conteinerBtnAcao.children[1].onclick = () => {
   location.reload();
 };
 
+
+form.addEventListener("submit", (event) => {
+  let entradaQR = event.target.entradaQr.value;
+  ativandoDownload(entradaQR);
+  event.target.reset();
+  event.preventDefault();
+});
+
+donwloadQrcodeBtn.addEventListener('click', ()=>{
+  if(!controle){
+    controle = confirm('Nenhum Qrcode foi gerado. Deseja baixar esse exmplo')
+  }
+  const dowload = imagenUrl => {
+    const elementDowload = document.createElement('a')
+    fetch(imagenUrl).then( async retornoURL =>{
+      const urlBlob = await retornoURL.blob()
+      elementDowload.href = URL.createObjectURL(urlBlob)
+      elementDowload.download = 'seu-qrcode'
+      elementDowload.click()
+      URL.revokeObjectURL(elementDowload.href)
+    })
+  } 
+  return controle ? dowload(imagenQrcode.src) : false
+})
 
